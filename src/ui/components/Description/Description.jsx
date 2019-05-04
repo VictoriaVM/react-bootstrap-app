2885b5c2f3ed778642ea6914f45da55f4f947dfe
VictoriaVM <victoria.v.movchan@gmail.com>
2019-05-04 21:08:26 +0300

    Правки в input.

--- src/ui/components/Description/Description.jsx
+++ src/ui/components/Description/Description.jsx
@@ -20,9 +20,17 @@ class Form extends Component {
         document.getElementById('glass').classList.remove(styles.glass);
         document.getElementById('form').classList.add(styles.form_change);
     };
+    outFormClickHandler (e) {
+        e.preventDefault();
+        document.getElementById('cross').classList.remove(styles.cross);
+        document.getElementById('cross').classList.add(styles.cross_change);
+        document.getElementById('glass').classList.remove(styles.glass_change);
+        document.getElementById('glass').classList.add(styles.glass);
+        document.getElementById('form').classList.remove(styles.form_change);
+    };
     render () {
         return <div className={styles.form} id='form'>
-            <form className='add cf' onClick={this.onFormClickHandler}>
+            <form className='add cf' onFocus={this.onFormClickHandler} onBlur={this.outFormClickHandler}>
                 <div className={styles.glass} role="button" tabIndex="0" id='glass'>
                     <span className={styles.SearchIcon}><img className={styles.SearchIcon} src="https://pngimage.net/wp-content/uploads/2018/06/%D0%B8%D0%BA%D0%BE%D0%BD%D0%BA%D0%B0-%D0%BB%D1%83%D0%BF%D0%B0-png-8.png"/></span>
                 </div>
